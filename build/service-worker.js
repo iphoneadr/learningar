cfd90e80d36f6a19b221bbdf60a1c15f717b0b5a
iphoneadr <56967281+iphoneadr@users.noreply.github.com>
2019-11-13 13:08:24 -0500

    Add files via upload

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d8c84e45ac5a67dab377a7a82b15a205.js"
+  "./precache-manifest.94957e1a92ae71dbb1a832a4ccd000b7.js"
 );
 
 self.addEventListener('message', (event) => {
